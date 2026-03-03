ff9ed03f7b48affa7c852729dbc469184b2863b7
kientt0308-tdmu <kientt0308@gmail.com>
2026-03-03 16:58:45 +0700

    Commit

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,8 +1,11 @@
+using MediatR;
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
